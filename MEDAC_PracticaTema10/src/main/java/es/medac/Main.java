a07e514ca396df0b1a9c46ddbe3234fb1af46b2c
JDan <juandanielromerobedoya@gmail.com>
2024-01-19 23:32:17 +0100

    Initial commit

--- MEDAC_PracticaTema10/src/main/java/es/medac/Main.java
+++ MEDAC_PracticaTema10/src/main/java/es/medac/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package medac_practicatema10;
+package main.java.es.medac;
 
 /**
  *
- * @author danie
+ * @author Daniel Ojados
  */
-public class main {
+public class Main {
 
     /**
      * @param args the command line arguments
