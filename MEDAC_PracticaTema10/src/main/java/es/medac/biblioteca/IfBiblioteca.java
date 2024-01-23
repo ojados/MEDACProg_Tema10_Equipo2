ae9ed9a47390e85d40a58d67bf1dd48afdd2f9b9
dani <dhg0003@alu.medac.es>
2024-01-23 16:51:38 +0100

    we add an interface called biblioteca that implements the methods prestarMaterial, devolverMaterial y mostrarInformacionPrestamo

---
+++ MEDAC_PracticaTema10/src/main/java/es/medac/biblioteca/IfBiblioteca.java
@@ -0,0 +1,23 @@
+package es.medac.biblioteca;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import es.medac.material.Material;
+
+
+public interface biblioteca {
+    /**
+     * Metodo prestar material que incluye un Array con Usuario y LocalDate
+      */
+void prestarMaterial(ArrayList<Material> material, Usuario user, LocalDate date);
+
+    /**
+     * Metodo devolver Material que incluye un Array con LocalDate
+     */
+    void devolverMaterial(ArrayList<Material> material, LocalDate date);
+
+    /**
+     * Metodo mostrar informacion de prestamo que no incluye nada
+     */
+    void mostrarInformacionPrestamo();
+}
