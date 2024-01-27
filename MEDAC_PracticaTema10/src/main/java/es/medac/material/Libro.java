ca5f7dfe9d561652886d1c5186b7b4912831b5ea
JDan <110744245+soyJDan@users.noreply.github.com>
2024-01-27 19:04:53 +0100

    Added documentation and refactor code.
    Documentation was added and the code was refactored to make it more readable. By default, when the return date is null, it is automatically set one month later.

--- MEDAC_PracticaTema10/src/main/java/es/medac/material/Libro.java
+++ MEDAC_PracticaTema10/src/main/java/es/medac/material/Libro.java
@@ -6,72 +6,121 @@
  */
 package es.medac.material;
 
-import es.medac.library.Biblioteca;
-
 import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import static es.medac.miscellany.LogLibrary.LOGGER;
 
 /**
- * The Libro class represents a book in the library system.
- * It extends the Material abstract class and implements the Prestable interface.
+ * The {@code Libro} class represents a book in the library system.
  *
- * <p>Each Libro object has an ISBN, title, author, year of publication, number of pages, and a borrowed status.</p>
+ * <p>This class extends the {@code Material} abstract class and implements the
+ * {@code Prestable} interface. It represents a book with properties such as
+ * number of pages and borrowed status.</p>
  *
- * <p>This class provides methods to show specific information about a book, check if the book is borrowed,
- * set and get the number of pages, lend the book, return the book, and a toString method to represent the state of the book.</p>
+ * <p>Example usage:</p>
+ * <blockquote><pre>
+ *     Libro libro = new Libro("123456", "Java Programming", "John Doe", 2020, 500);
+ *     libro.prestar();
+ *     libro.devolver();
+ * </pre></blockquote>
  *
- * @author Anouar Mais
- * @version 1.0
+ * @since 1.0
+ *
  * @see Material
  * @see Prestable
+ *
+ * @author Anouar Mais (Anouarmais)
+ *
+ * @version 1.0
  */
 public class Libro extends Material {
 
-    private static final Logger LOGGER = Logger.getLogger(Libro.class.getName());
-
+    /**
+     * The number of pages in the book.
+     */
     private int numPages;
+
+    /**
+     * The borrowed status of the book.
+     */
     private boolean borrowed;
 
+    /**
+     * Constructs a new book with the specified ISBN, title, author, year
+     * of publication, and number of pages.
+     *
+     * @param isbn the ISBN of the book
+     * @param title the title of the book
+     * @param author the author of the book
+     * @param yearPublication the year of publication of the book
+     * @param numPages the number of pages in the book
+     */
     public Libro(String isbn, String title, String author, int yearPublication, int numPages) {
         super(isbn, title, author, yearPublication);
         this.numPages = numPages;
         this.borrowed = false;
     }
 
+    /**
+     * Returns the number of pages in the book.
+     *
+     * @return the number of pages in the book
+     */
     public int getNumPages() {
         return numPages;
     }
 
+    /**
+     * Sets the number of pages in the book.
+     *
+     * @param numPages the new number of pages in the book
+     */
     public void setNumPages(int numPages) {
         this.numPages = numPages;
     }
 
+    /**
+     * Returns the borrowed status of the book.
+     *
+     * @return the borrowed status of the book
+     */
     public boolean isBorrowed() {
         return borrowed;
     }
 
+    /**
+     * Sets the borrowed status of the book.
+     *
+     * @param borrowed the new borrowed status of the book
+     */
     public void setBorrowed(boolean borrowed) {
         this.borrowed = borrowed;
     }
 
+    /**
+     * Borrows the book.
+     *
+     * <p>This method sets the borrowed status of the book to true.</p>
+     */
     @Override
     public void prestar() {
         if (isBorrowed()) {
             LOGGER.log(Level.WARNING, "El libro ya está prestado");
         } else {
-            LOGGER.log(Level.INFO, Biblioteca.ANSI_CYAN + "{0}" + Biblioteca.ANSI_RESET, this);
-            LOGGER.log(Level.INFO, Biblioteca.ANSI_CYAN + "Préstamo realizado." + Biblioteca.ANSI_RESET);
             borrowed = true;
         }
     }
 
+    /**
+     * Returns the book.
+     *
+     * <p>This method sets the borrowed status of the book to false.</p>
+     */
     @Override
     public void devolver() {
         if (!isBorrowed()) {
             LOGGER.log(Level.WARNING, "El libro no está prestado");
         } else {
-            LOGGER.log(Level.INFO, Biblioteca.ANSI_CYAN + "{0}" + Biblioteca.ANSI_RESET, this);
-            LOGGER.log(Level.INFO, Biblioteca.ANSI_CYAN + "Devolución realizada." + Biblioteca.ANSI_RESET);
             borrowed = false;
         }
     }
@@ -81,12 +130,17 @@ public class Libro extends Material {
      */
     @Override
     public void mostrarInformacionEspecifica() {
-        System.out.println("Muestrando informacion especifica: ");
-        System.out.println("Título: " + getTitle());
-        System.out.println("Autor: " + getAuthor());
-        System.out.println("ISBN: " + getIsbn());
+        LOGGER.log(Level.INFO, "Muestrando informacion especifica: ");
+        LOGGER.log(Level.INFO, "Título: {0}", getTitle());
+        LOGGER.log(Level.INFO, "Autor: {0}", getAuthor());
+        LOGGER.log(Level.INFO, "ISBN: {0}", getIsbn());
     }
 
+    /**
+     * Returns a string representation of the book.
+     *
+     * @return a string representation of the book
+     */
     @Override
     public String toString() {
         return "Libro\n" +
