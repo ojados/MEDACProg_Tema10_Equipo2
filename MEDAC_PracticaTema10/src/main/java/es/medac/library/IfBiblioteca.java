cbbf16cd757edd79a7a4d47a3ad9d7ff57935adb
JDan <110744245+soyJDan@users.noreply.github.com>
2024-01-27 18:41:52 +0100

    Added documentation.

--- MEDAC_PracticaTema10/src/main/java/es/medac/library/IfBiblioteca.java
+++ MEDAC_PracticaTema10/src/main/java/es/medac/library/IfBiblioteca.java
@@ -4,21 +4,41 @@ import es.medac.material.Material;
 import es.medac.users.Usuario;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 /**
- * This class is a interface that have its methods.
+ * This interface represents the operations that a library can perform.
  *
+ * @since 1.0
  *
+ * @see Biblioteca
+ *
+ * @author Daniel Romero (JDan)
  * @author Katerine Hidalgo (Katehlo0412)
  * @author Daniel Hernandez (dhernandeez13)
- * @author Daniel Romero (JDan)
+ *
+ * @version 1.1
  */
 public interface IfBiblioteca {
 
+    /**
+     * This method is used to lend a material to a user.
+     *
+     * @param material The material to be lent.
+     * @param user The user who is borrowing the material.
+     * @param date The date when the material is being borrowed.
+     */
     void prestarMaterial(Material material, Usuario user, LocalDate date);
 
+    /**
+     * This method is used to return a material to the library.
+     *
+     * @param material The material to be returned.
+     * @param date The date when the material is being returned.
+     */
     void devolverMaterial(Material material, LocalDate date);
 
+    /**
+     * This method is used to display information about the loans.
+     */
     void mostrarInformacionPrestamos();
 }
