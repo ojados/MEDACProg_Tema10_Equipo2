90d16010cf159884ab8454ffb612549afe4ce2c3
JDan <110744245+soyJDan@users.noreply.github.com>
2024-02-03 19:30:09 +0100

    Write documentation

--- MEDAC_PracticaTema10/src/main/java/es/medac/exceptions/PrestamoVencidoException.java
+++ MEDAC_PracticaTema10/src/main/java/es/medac/exceptions/PrestamoVencidoException.java
@@ -1,7 +1,24 @@
-package main.java.es.medac.exceptions;
+package es.medac.exceptions;
 
-public class PrestamoVencidoException extends Exception{
 
+/**
+ * The {@code PrestamoVencidoException} class represents an exception that is thrown when a loan is overdue.
+ *
+ * @since 1.0
+ *
+ * @see Exception
+ *
+ * @version 1.0
+ * @author Anouar Mais (Anouarmais)
+ * @author Daniel Romero (JDan)
+ */
+public class PrestamoVencidoException extends Exception {
+
+    /**
+     * Constructs a new {@code PrestamoVencidoException} with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public PrestamoVencidoException(String message) {
         super(message);
 
